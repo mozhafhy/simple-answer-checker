e1d1ebd7186858514e8381ae75106eb9c668bae1
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-14 19:22:05 +0700

    refactored some js

--- App.js
+++ App.js
@@ -10,11 +10,10 @@ const [result] = document.getElementsByClassName("result");
 totalSoal.textContent = answers.length;
 
 let count = 1;
+// ! Submit button
 submit.addEventListener("click", () => {
-  const userInput = input.value.trim().toLowerCase();
+  const userInput = input.value;
   result.innerHTML = "";
-
-  console.log(`Inputmu ${userInput}, bro`);
 
   const showResult = document.createElement("div");
   showResult.className = "show-result";
@@ -22,32 +21,28 @@ submit.addEventListener("click", () => {
   if (userInput === answers[count - 1]) {
     showResult.innerHTML = "TRUE";
     showResult.style.background = "green";
-    console.log("Jawaban benar");
   } else {
     showResult.innerHTML = "FALSE";
     showResult.style.background = "red";
-    console.log("Jawaban salah");
   }
-  console.log(showResult);
 
   result.appendChild(showResult);
-  
-  setTimeout(() => {
-    showResult.classList.add("in");
-  }, 0);
+
+  showResult.classList.add("show-result");
+
+  void showResult.offsetHeight;
+
+  showResult.classList.add("in");
 
   setTimeout(() => {
-    showResult.classList.remove("in");
-    showResult.classList.add("out");
+    showResult.classList.replace("in", "out");
+    setTimeout(() => {
+      showResult.remove();
+    }, 2000);
   }, 1000);
-
-  setTimeout(() => {
-    showResult.remove();
-  }, 2000);
-
-  console.log(`Ini jawabannya ${answers[count - 1]}`);
 });
 
+// ! Next button
 next.addEventListener("click", () => {
   if (count < answers.length) {
     count++;
