2e560a7c41fa50ef240c69e1af31b0234a6161df
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-17 18:52:02 +0700

    finished answer-checking js for Ronde3.html

--- babak/babak1/ronde-3/Ronde3.js
+++ babak/babak1/ronde-3/Ronde3.js
@@ -1,69 +1,22 @@
-const answers = ["a", "b", "c", "d", "e"];
+const answer = ["a", "b", "c"];
 
-const [next] = document.getElementsByClassName("next");
-const [back] = document.getElementsByClassName("back");
-const [counter] = document.getElementsByClassName("counter");
-const [input] = document.getElementsByClassName("jawab");
-const [totalSoal] = document.getElementsByClassName("count");
-const [submit] = document.getElementsByClassName("submit");
-const [result] = document.getElementsByClassName("result");
+const els = document.querySelectorAll("form");
+const soal = ["A", "B", "C"];
 
-totalSoal.textContent = answers.length;
+els.forEach((el, key) => {
+  let [jawab, submit] = el.children;
 
-let count = 1;
-submit.addEventListener("click", () => {
-  const userInput = input.value;
-  result.innerHTML = "";
+  submit.addEventListener("click", () => {
+    let [box] = document.getElementsByClassName(soal[key]);
+    // box.style.background = "";
+    if (jawab.value === answer[key]) {
+      box.style.background = "green";
+    } else {
+      box.style.background = "red";
+    }
 
-  console.log(`Inputmu ${userInput}, bro`);
-
-  const showResult = document.createElement("div");
-  showResult.className = "show-result";
-
-  if (userInput === answers[count - 1]) {
-    showResult.innerHTML = "TRUE";
-    showResult.style.background = "green";
-    console.log("Jawaban benar");
-  } else {
-    showResult.innerHTML = "FALSE";
-    showResult.style.background = "red";
-    console.log("Jawaban salah");
-  }
-  console.log(showResult);
-
-  result.appendChild(showResult);
-  
-  setTimeout(() => {
-    showResult.classList.add("in");
-  }, 0);
-
-  setTimeout(() => {
-    showResult.classList.remove("in");
-    showResult.classList.add("out");
-  }, 1000);
-
-  setTimeout(() => {
-    showResult.remove();
-  }, 2000);
-
-  console.log(`Ini jawabannya ${answers[count - 1]}`);
-});
-
-next.addEventListener("click", () => {
-  if (count < answers.length) {
-    count++;
-    counter.textContent = count;
-  } else {
-    alert(`Hanya ada ${answers.length} soal!`)
-    return;
-  }
-});
-back.addEventListener("click", () => {
-  if (count > 1) {
-    count--;
-    counter.textContent = count;
-  } else {
-    alert("Anda berada di soal nomor 1!")
-    return;
-  }
+    setTimeout(() => {
+      box.style.background = "";
+    }, 2000);
+  });
 });
