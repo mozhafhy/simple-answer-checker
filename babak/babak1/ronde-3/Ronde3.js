0f9fd5e06b8d1b75cf0ebe8b791cedc11ef46e77
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-17 19:03:15 +0700

    added more styling to Ronde3.js

--- babak/babak1/ronde-3/Ronde3.js
+++ babak/babak1/ronde-3/Ronde3.js
@@ -11,12 +11,18 @@ els.forEach((el, key) => {
     // box.style.background = "";
     if (jawab.value === answer[key]) {
       box.style.background = "green";
+      el.style.pointerEvents = "none";
+      el.style.filter = "opacity(0.5) brightness(0.75)"
     } else {
       box.style.background = "red";
+      el.style.pointerEvents = "none";
+      el.style.filter = "opacity(0.5) brightness(0.75)"
     }
-
+    
     setTimeout(() => {
       box.style.background = "";
+      el.style.pointerEvents = "";
+      el.style.filter = ""
     }, 2000);
   });
 });
