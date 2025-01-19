51a30cce66afaafd9760255b6b0de09ea77b993b
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-19 19:26:10 +0700

    refactored ronde-2 html, css, and js

--- babak/babak1/ronde-2/Ronde2.js
+++ babak/babak1/ronde-2/Ronde2.js
@@ -1,69 +1,68 @@
 const answers = ["a", "b", "c", "d", "e"];
 
-const [next] = document.getElementsByClassName("next");
-const [back] = document.getElementsByClassName("back");
-const [counter] = document.getElementsByClassName("counter");
-const [input] = document.getElementsByClassName("jawab");
-const [totalSoal] = document.getElementsByClassName("count");
-const [submit] = document.getElementsByClassName("submit");
-const [result] = document.getElementsByClassName("result");
+const next = document.querySelector(".next");
+const back = document.querySelector(".back");
+const counter = document.querySelector(".counter");
+const input = document.querySelector(".jawab");
+const totalSoal = document.querySelector(".count");
+const submit = document.querySelector(".submit");
+const result = document.querySelector(".result");
+const message = document.querySelector(".alert");
 
 totalSoal.textContent = answers.length;
 
 let count = 1;
+const ANIMATION_DURATION = 1250;
 submit.addEventListener("click", () => {
   const userInput = input.value;
-  result.innerHTML = "";
-
-  console.log(`Inputmu ${userInput}, bro`);
+  result.textContent = "";
 
   const showResult = document.createElement("div");
   showResult.className = "show-result";
-
-  if (userInput === answers[count - 1]) {
-    showResult.innerHTML = "TRUE";
-    showResult.style.background = "green";
-    console.log("Jawaban benar");
-  } else {
-    showResult.innerHTML = "FALSE";
-    showResult.style.background = "red";
-    console.log("Jawaban salah");
-  }
-  console.log(showResult);
+  showResult.textContent = userInput === answers[count - 1] ? "BENAR" : "SALAH";
+  showResult.classList.add(
+    userInput === answers[count - 1] ? "benar" : "salah"
+  );
 
   result.appendChild(showResult);
-  
-  setTimeout(() => {
-    showResult.classList.add("in");
-  }, 0);
 
-  setTimeout(() => {
-    showResult.classList.remove("in");
-    showResult.classList.add("out");
-  }, 1000);
-
-  setTimeout(() => {
-    showResult.remove();
-  }, 2000);
-
-  console.log(`Ini jawabannya ${answers[count - 1]}`);
+  setTimeout(() => showResult.classList.add("in"), 0); // ? The .in class appears instantly
+  setTimeout(
+    () => showResult.classList.replace("in", "out"),
+    ANIMATION_DURATION
+  );
+  setTimeout(() => showResult.remove(), ANIMATION_DURATION * 2);
 });
+
+const MESSAGE_DURATION = 2000;
+let messageTimeOut;
+function showMessage(text, duration = MESSAGE_DURATION) {
+  if (messageTimeOut) {
+    clearTimeout(messageTimeOut);
+  }
+
+  message.textContent = text;
+  messageTimeOut = setTimeout(() => {
+    message.textContent = "";
+    messageTimeOut = null;
+  }, duration);
+}
 
 next.addEventListener("click", () => {
   if (count < answers.length) {
     count++;
+    message.textContent = "";
     counter.textContent = count;
   } else {
-    alert(`Hanya ada ${answers.length} soal!`)
-    return;
+    showMessage("Anda berada di soal terakhir!");
   }
 });
 back.addEventListener("click", () => {
   if (count > 1) {
     count--;
+    message.textContent = "";
     counter.textContent = count;
   } else {
-    alert("Anda berada di soal nomor 1!")
-    return;
+    showMessage("Anda berada di soal pertama!");
   }
 });
