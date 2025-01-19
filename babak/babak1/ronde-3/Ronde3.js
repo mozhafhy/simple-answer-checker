458ba45049b30bfab71bf65c2231d01f7d415a03
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-19 14:35:51 +0700

    added teams' progress board

--- babak/babak1/ronde-3/Ronde3.js
+++ babak/babak1/ronde-3/Ronde3.js
@@ -1,28 +1,39 @@
 const answer = ["a", "b", "c"];
 
-const els = document.querySelectorAll("form");
+const inputs = document.querySelectorAll("form");
 const soal = ["A", "B", "C"];
 
-els.forEach((el, key) => {
-  let [jawab, submit] = el.children;
+const [tim] = document.getElementsByClassName("nomor");
+const [scoreboard] = document.getElementsByClassName("scoreboard");
+const progresses = scoreboard.children;
+
+inputs.forEach((input, key) => {
+  const [jawab, submit] = input.children;
 
   submit.addEventListener("click", () => {
-    let [box] = document.getElementsByClassName(soal[key]);
-    // box.style.background = "";
+    if (tim.value === "") {
+      alert("Masukkan Nomor Tim!");
+      return;
+    }
+
+    const nomor = Number(tim.value);
+    const [_, progress] = progresses[nomor - 1].children;
+    const terjawab = progress.children;
+
+    const [box] = document.getElementsByClassName(soal[key]);
     if (jawab.value === answer[key]) {
+      terjawab[key].style.background = "green";
       box.style.background = "green";
-      el.style.pointerEvents = "none";
-      el.style.filter = "opacity(0.5) brightness(0.75)"
     } else {
       box.style.background = "red";
-      el.style.pointerEvents = "none";
-      el.style.filter = "opacity(0.5) brightness(0.75)"
     }
-    
+    input.style.filter = "opacity(0.5) brightness(0.75)";
+    input.style.pointerEvents = "none";
+
     setTimeout(() => {
       box.style.background = "";
-      el.style.pointerEvents = "";
-      el.style.filter = ""
+      input.style.pointerEvents = "";
+      input.style.filter = "";
     }, 2000);
   });
 });
